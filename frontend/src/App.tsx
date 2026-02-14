540bb1aee07d616ef2749396f051ec6cf372ce30
GrzegorzSad <N00223252@iadt.ie>
2026-02-14 16:54:15 +0000

    new front base on vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      
+        <Button variant="default" size="lg">Primary</Button>
+    </>
+  )
+}
+
+export default App
