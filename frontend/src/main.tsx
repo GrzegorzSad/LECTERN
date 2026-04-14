9fb01358d1ea67db58ee1b021a590e00d679a43c
GrzegorzSad <N00223252@iadt.ie>
2026-04-14 15:33:08 +0100

    logo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,25 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { GroupsProvider } from "./context/GroupsContext";
+import { useDarkMode } from "./hooks/useDarkMode";
+
+function AppProviders() {
+  const { isDark } = useDarkMode();
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDark);
+  }, [isDark]);
+  return <RouterProvider router={router} />;
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <GroupsProvider>
-        <RouterProvider router={router} />
+        <AppProviders />
       </GroupsProvider>
     </AuthProvider>
   </StrictMode>,
